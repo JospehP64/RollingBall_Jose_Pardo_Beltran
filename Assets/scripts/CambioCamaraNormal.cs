effd0780536641c2fbcfacdf2b0e648818827fce
JospehP64 <jmjakepardo@gmail.com>
2024-10-21 10:42:47 +0200

    21-10-2024
    -Se ha creado un nuevo escenario para el nivel del juego de prueba (y se ha hecho una copia del escenario original en caso de ser necesario)
    
    -Se han creado varios prefabs para ser usados en el nuevo escenario
    
    -Se han añadido materiales nuevos para cada objeto
    
    -Ahora, cuando la bola llegue a cierto punto de la escena en cámara cenital,  volverá a la cámara normal, creando un cambio entre cámaras

--- Assets/scripts/CambioCamaraNormal.cs
+++ Assets/scripts/CambioCamaraNormal.cs
@@ -2,21 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cambioDe_Camara : MonoBehaviour
+public class CambioCamaraNormal : MonoBehaviour
 {
     [SerializeField] GameObject CambiarACenital;
     [SerializeField] GameObject MainCamera;
     // Start is called before the first frame update
     void Start()
     {
-        CambiarACenital.SetActive(false);
-        MainCamera.SetActive(true);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -25,11 +24,11 @@ public class cambioDe_Camara : MonoBehaviour
         //MainCamera.SetActive(false);
         if (other.gameObject.CompareTag("Player"))
         {
-            CambiarACenital.SetActive(true);
-            MainCamera.SetActive(false);
+            CambiarACenital.SetActive(false);
+            MainCamera.SetActive(true);
 
         }
-        
+
 
 
     }
