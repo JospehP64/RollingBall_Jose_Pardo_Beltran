88d4b18b34b1d3cfbf749a40af08608edb0182c5
JospehP64 <jmjakepardo@gmail.com>
2024-11-07 13:33:56 +0100

    07-11-2024
    -Se han hecho cambios en el código para el "Life Manager".
    -Se han hecho cambios menores en el nivel del juego
    -AUN QUEDA PENDIENTE TRABAJAR EN LA INTERFAZ DE VIDA. LA FUNCIÓN EL CÓDIGO NO ESTÁ COMPLETADA.

--- Assets/scripts/LifeGuiManager.cs
+++ Assets/scripts/LifeGuiManager.cs
@@ -4,36 +4,56 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
+
 public class LifeGuiManager : MonoBehaviour
 {
     [SerializeField] RawImage heart_ui_component;
     [SerializeField] RawImage Second_heart_ui_component;
     [SerializeField] RawImage Third_heart_ui_component;
+    [SerializeField] Texture[] heart_texture; 
     movimiento_esfera esfera;
     // Start is called before the first frame update
     void Start()
     {
+        heart_ui_component = GetComponent<RawImage>();
         
+        heart_ui_component.texture = (heart_texture[0])as Texture;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        LifeArray();
+    }
+
+    void LifeArray()
+    {
+
         if (esfera.Vida >= 3)
         {
-            //heart_ui_component.CORREGIR
+            heart_ui_component.texture = heart_texture[0];
+            Second_heart_ui_component.texture = heart_texture[0];
+            Third_heart_ui_component.texture = heart_texture[0];
         }
         else if (esfera.Vida >= 2 && esfera.Vida < 3)
         {
-
+            heart_ui_component.texture = heart_texture[2];
+            Second_heart_ui_component.texture = heart_texture[1];
+            Third_heart_ui_component.texture = heart_texture[1];
         }
         else if (esfera.Vida >= 1 && esfera.Vida < 2)
         {
-
+            heart_ui_component.texture = heart_texture[1];
+            Second_heart_ui_component.texture = heart_texture[1];
+            Third_heart_ui_component.texture = heart_texture[0];
         }
-        else if (esfera.Vida <= 0 )
+        else if (esfera.Vida <= 0)
         {
-
+            heart_ui_component.texture = heart_texture[1];
+            Second_heart_ui_component.texture = heart_texture[1];
+            Third_heart_ui_component.texture = heart_texture[1];
         }
+
     }
 }
